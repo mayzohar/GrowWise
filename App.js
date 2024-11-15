bcc9132ba4534d4b89f57b988d6ee7b77abef06a
sapirovadya <sapirov21@gmail.com>
2024-11-15 15:07:11 +0200

    GrowWise App Upload

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello sapir</Text>
+      <Text>GrowWise App</Text>
       <StatusBar style="auto" />
     </View>
   );
