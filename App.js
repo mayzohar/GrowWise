04de3a1405bc3b53338c35f1cc27444ca15c1e81
sapirovadya <sapirov21@gmail.com>
2024-11-16 22:12:38 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>GrowWise App</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
 import React from 'react';
 import AppNavigator from './components/AppNavigator';
 
